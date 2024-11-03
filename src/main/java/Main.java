6a8ee7bbd64b7e4bfa8ccfae8f6abc01509f9046
yoavdror08 <yoavdror2008@gmail.com>
2024-11-03 11:01:35 +0200

    recurcia2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,72 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+package recurcia2;
+
+public class homework {
+	//ex14
+	public static int sum(int [] arr, int n) {
+		if(n==0)
+			return arr[0];
+		return sum(arr,n-1)+arr[n];
+		
+	}
+	//ex15
+	public static int positivenum(int [] arr, int n) {
+        if (n < 0) {
+            return 0;
+        }
+        if (arr[n] > 0) {
+            return 1 + positivenum(arr, n - 1);
+        } else {
+            return positivenum(arr, n - 1);
+        }
+	}
+	//ex16
+	public static int placeArr(int [] arr, int n,int index) {
+        if (index >= arr.length) {
+            return -1;
+        }
+        if (arr[index] == n) {
+            return index;
+        }
+        return placeArr(arr, n, index + 1);
+	}
+	//ex17
+	public static boolean AscendingOrder(int [] arr,int n,int index) {
+		
+	
+	    if (arr[index] > arr[index + 1]) 
+	        return false;
+			
+	    if (index >= arr.length - 1) 
+	         return true;
+	    
+        return AscendingOrder(arr, arr[index + 1], index + 1);
+	
+	}
+	//ex18
+    public static boolean hasNoPrimes(int[] arr, int index) {
+        if (index >= arr.length) {
+            return true;
+        }
+        if (isPrime(arr[index])) {
+            return false;
+        }
+        return hasNoPrimes(arr, index + 1);
+    }
+	
+	
+	
+	
+    private static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false; 
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false; 
+            }
+        }
+        return true;
+    }
+	
+	
 }
